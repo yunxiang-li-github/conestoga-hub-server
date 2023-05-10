3384b3c3aca4c82fe07747a1b8227b56979d2750
yunxiang-li-github <yli4830@conestogac.on.ca>
2023-05-10 13:54:03 -0400

    try the other vercel json

--- config/db.js
+++ config/db.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
+import http from 'http';
 // const config = require('config');
 require('dotenv').config();
 const db = process.env.MONGO_URI; // || config.get('mongoURI');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer(app);
 
 const connectDB = async () => {
-  try{
+  try {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(db);
-    console.log('MongoDB Connected...');
-  } catch(err){
+    await mongoose.connect(db).then(() => {
+      console.log('MongoDB Connected...');
+      server.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+      });
+    });
+  } catch (err) {
     console.error(err.message);
     // exit process with failure
     process.exit(1);
   }
-}
+};
 module.exports = connectDB;
